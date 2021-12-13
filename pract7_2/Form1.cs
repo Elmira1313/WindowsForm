f50dc96538b87ce627a61aa4ee05606170e753e1
Elmira1313 <95313030+Elmira1313@users.noreply.github.com>
2021-12-13 21:56:05 +0300

    Add files via upload

---
+++ pract7_2/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+
+namespace pract7_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            richTextBox2.Text = "";
+            char x;
+            StringBuilder a = new StringBuilder();
+            if (Char.TryParse(textBox1.Text, out x))
+            {
+                a.Append(richTextBox1.Text);
+                try
+                {
+                    bool flag = true;
+                    for (int i = 0; i < a.Length; ++i)
+                    {
+                        if (a[i] == x)
+                        {
+                            a.Insert(i + 1, x);
+                            ++i;
+                        }
+                        else
+                        {
+                            flag = false;
+                        }
+                    }
+                    if (flag == true)
+                    {
+                        richTextBox2.Text = $"{a}";
+                    }
+                    else
+                    {
+                        richTextBox2.Text = "";
+                        MessageBox.Show("Такого символа нет!");
+                    }
+                }
+                catch (Exception)
+                {
+                    richTextBox2.Text = "";
+                    MessageBox.Show("Что-то введено не так!");
+                }
+            }
+            else
+            {
+                richTextBox2.Text = "";
+                MessageBox.Show("Что-то введено не так!");
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form2 form2 = new Form2();
+            form2.Show();
+        }
+    }
+}
